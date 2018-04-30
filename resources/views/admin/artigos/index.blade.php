dcf95abb6377281e2f2662de36b80945099d8620
Henrique <henriquedin@hotmail.com>
2018-04-29 21:24:31 -0300

    Componente modal

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -4,7 +4,11 @@
 
 <pagina tamanho="12">
     <painel titulo="Lista de Artigos">
-        <migalhas v-bind:lista="{{$listaMigalhas}}"></migalhas>        
+    
+        <migalhas v-bind:lista="{{$listaMigalhas}}"></migalhas>
+        
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#meuModalTeste">Large modal</button>
+
         <tabela-lista 
         v-bind:titulos="['#', 'Título','Descrição']"
         v-bind:itens="[[1,'PHP OO', 'Curso de PHP - OO'],[2, 'Vue JS', 'Curso de Vue JS']]"
@@ -12,5 +16,31 @@
         ></tabela-lista>
     </painel>
 </pagina>
+
+<modal nome="meuModalTeste">
+    <painel titulo="Adicionar">
+        <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputFile">File input</label>
+            <input type="file" id="exampleInputFile">
+            <p class="help-block">Example block-level help text here.</p>
+        </div>
+        <div class="checkbox">
+            <label>
+            <input type="checkbox"> Check me out
+            </label>
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+    </painel>
+</modal>
 
 @endsection
